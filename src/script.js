61935fab117e9a1497ec50af6754026ff09eab79
Jason Kao <jason.kao@columbia.edu>
2020-03-30 09:52:16 -0400

    serverside rendered spectate stylesheets dont override site styles on contributor pages

--- src/script.js
+++ src/script.js
@@ -29,7 +29,23 @@ export function hamburgerTrigger() {
 
 textBalancer.balanceText('.headline, .deck, .image-overlay .image-caption-text');
 
-var $ = require("../node_modules/jquery");
+// Disable stylesheets on contributor page. Stylesheets are included in the
+// server-side render and styles in files like news-ellipsis.scss
+// still override the site.
+
+if (window.location.pathname.indexOf('/contributors') === 0) {
+  // Disable stylesheets
+  document.querySelectorAll('.story-summary > .twolines > link').forEach(link => link.disabled = true);
+  // Add styles to hide content preview
+  const styleSheet = document.createElement("style");
+  styleSheet.type = "text/css"
+  styleSheet.innerText = '.story-summary > .twolines { display: none; }';
+  document.head.appendChild(styleSheet);
+}
+
+// Slides logic
+
+import $ from 'jquery';
 
 $(document).ready(function () {
   //initialize swiper when document ready
